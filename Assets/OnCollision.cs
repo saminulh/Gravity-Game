7a3c4a918ef1b8f96f0c85408d78f49e55b7d50a
saminulh <saminulh@hotmail.com>
2016-12-16 23:17:58 -0500

    upload game

---
+++ Assets/OnCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollision : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (GameManager.isLaunched)
+        {
+            if (collision.gameObject.name == "End Planet")
+            {
+                GameManager.win();
+            }
+            else
+            {
+                GameManager.fail();
+            }
+        }
+    }
+}
